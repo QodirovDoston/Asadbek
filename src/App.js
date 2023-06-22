e2e2e19be134c3ba374b59221f9fa8252350c48b
qodirovdoston806 <qodirovdoston806@gmail.com>
2023-06-22 08:10:04 +0500

    dsD

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import './App.css';
 import Analytice from './Componets/Analytice';
+import Cards from './Componets/Card';
 import Hero from './Componets/Hero';
 import Navbar from './Componets/Navbar';
 import NewsLatter from './Componets/NewsLatter';
+import Footer from './Componets/Footer'
 
 function App() {
   return (
     <>
-     <Navbar/>
-     <Hero/>
-     <Analytice/>
-     <NewsLatter/>
+      <Navbar />
+      <Hero />
+      <Analytice />
+      <Cards />
+      <NewsLatter />
+      <Footer />
     </>
   );
 }
