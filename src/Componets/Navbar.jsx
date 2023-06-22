e516c7742deb296aa2d2a4b0e2ccd5369917b380
qodirovdoston806 <qodirovdoston806@gmail.com>
2023-06-22 09:08:13 +0500

    defsxdf

--- src/Componets/Navbar.jsx
+++ src/Componets/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
                 {!nav ? <AiOutlineClose size={50}/> : <AiOutlineMenu size={50} />}
             </div>
             <div className={!nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#021102] ease-in-out duration-500': 'fixed left-[-100%]'}>
-                <h3 className='w-full text-3xl font-bold text-[#134ce6] m-4 '>Dostonjon.</h3>
+                <h3 className='w-full text-3xl font-bold text-[#134ce6] m-4 '>Asadbekjon.</h3>
                 <ul className='p-4 uppercase'>
                     <li className='p-4 border-b border-gray-600'>home</li>
                     <li className='p-4 border-b border-gray-600'>company</li>
